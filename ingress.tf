6d29e87419bab6da46db0515901dde9817d66369
Denis Krupin <taigusiobaka@gmail.com>
2021-12-12 21:00:52 +0300

    Add ingress to infrastructure

---
+++ ingress.tf
@@ -0,0 +1,38 @@
+variable "ingress-namespace" {
+  type    = string
+  default = "ingress"
+}
+
+resource "kubernetes_namespace" "ingress" {
+  metadata {
+    name = var.ingress-namespace
+  }
+}
+
+resource "helm_release" "nginx_ingress" {
+  name      = "nginx-ingress-controller"
+  namespace = var.ingress-namespace
+
+  repository = "https://helm.nginx.com/stable"
+  chart      = "nginx-ingress"
+
+  set {
+    name  = "controller.service.type"
+    value = "NodePort"
+  }
+
+  set {
+    name  = "controller.setAsDefaultIngress"
+    value = "true"
+  }
+
+  set {
+    name  = "controller.service.httpPort.nodePort"
+    value = "30080"
+  }
+
+  set {
+    name  = "controller.service.httpsPort.nodePort"
+    value = "30443"
+  }
+}
