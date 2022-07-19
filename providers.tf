54235ad46d8df33b7a4cece5e7b3fa65931c4f3a
Denis Krupin <taigusiobaka@gmail.com>
2022-07-19 06:37:52 +0300

    Feature: Add minio provider

--- providers.tf
+++ providers.tf
@@ -7,11 +7,19 @@ variable "database-port" {
   default = 30432
 }
 
+variable "minio-endpoint" {
+  type = string
+}
+
 terraform {
   required_providers {
     postgresql = {
       source  = "cyrilgdn/postgresql"
       version = "1.16.0"
+    }
+    minio = {
+      source = "refaktory/minio"
+      version = "0.1.0"
     }
   }
 }
@@ -32,4 +40,11 @@ provider "postgresql" {
   username = var.postgres-admin-username
   password = var.postgres-admin-password
   sslmode  = "disable"
+}
+
+provider "minio" {
+  endpoint = var.minio-endpoint
+  access_key = var.minio-admin-user
+  secret_key = var.minio-admin-pass
+  ssl = false
 }
